c199877c146d8d75e86f1d1619b2a50041d0840a
Brian Gorman <blgorman@gmail.com>
2022-07-20 13:30:36 -0500

    Update Program.cs
    Clean this up and add the database migration ability, as well as more explicit instructions on utilization of app config and keyvault

--- DN6SimpleWebWithAuth/Program.cs
+++ DN6SimpleWebWithAuth/Program.cs
@@ -16,16 +16,41 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddControllersWithViews();
 builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
 
-/* NOTE: To enable the use of AppConfiguration and Keyvault integration, uncomment the following code,
- *          and ENSURE you have a system managed identity for both app services at Azure,
- *          then also add all of the app service and slots to the App Configuration with Configuration Data Reader Role
- * Additional NOTE: If using KeyVault, ensure additional identity on Azure App Config, and then ensure all of the identities can get keyvault secret
- *                      through access policies on your keyvault for Get Secret for App Config, App Service, and all App Service Slots.
- *                  ,Then uncomment Keyvault code
- *                  You will also need to remove the `); from the connect line to chain the command, and push.
- *                  also note that your devs will also need access to keyvault get secret in order to work with this locally through default credential
- * Final Note: For the code to work, ensure you add NuGet packages for Azure.Identity and Microsoft.Extensions.Configuration.AzureAppConfiguration
+// OPTIONALLY: enable automatic migrations
+// *************
+// NOTE: IF you do this, you will never be able to roll-back a migration with this code in place.  Use at your own discretion:
+// **************
+/*
+//automatically apply database migrations (breaks solution if database not wired up correctly, forces roll-forward approach
+var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer(connectionString).Options;
+using (var context = new ApplicationDbContext(contextOptions))
+{
+    context.Database.Migrate();
+}
+*/
 
+/* 
+ * App Configuration and KeyVault integration: 
+ *      To enable the use of AppConfiguration and Keyvault integration, uncomment the following code, using the version of credential without the keyvalt configured
+ *      - ENSURE you have a system managed identity for your App Service(s)
+ *      - Add the default url for the Azure App Config as a configuration variable 
+ *          i.e. 
+ *              AzureAppConfigConnection 
+ *              https://someazureappconfignamehere.azconfig.io
+ *      - For the developer, add the secret in your secret config but use the full connection string to the azure app configuration
+ *      - Add all of the app service(s) and slots to the App Configuration with Configuration Data Reader Role
+ *      - Add your developer to the app service with data reader role
+ *
+ * Additional NOTE: If using KeyVault, 
+ *      - ENSURE you have a system managed identity for your App Service(s) and your Azure App COnfig
+ *      - ENSURE you have an access policy on your key vault for the Azure App Config and your App Service(s)
+ *      - You will also need to switch the config generation to the keyvault version for each environment below.
+ *      - Finally, note that your devs will also need access to keyvault get secret in order to work with this locally through the default credential
+ * 
+ * Final Note: For the code to work, ensure you have NuGet packages for Azure.Identity and Microsoft.Extensions.Configuration.AzureAppConfiguration (they are already added to this project)
+ *
+*/
+/*
 builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
 {
     var settings = config.Build();
@@ -35,14 +60,22 @@ builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
         var cred = new ManagedIdentityCredential();
         config.AddAzureAppConfiguration(options =>
                 options.Connect(new Uri(settings["AzureAppConfigConnection"]), cred));
-                            //.ConfigureKeyVault(kv => { kv.SetCredential(cred); }));
+        /*
+        config.AddAzureAppConfiguration(options =>
+                options.Connect(new Uri(settings["AzureAppConfigConnection"]), cred)
+                            .ConfigureKeyVault(kv => { kv.SetCredential(cred); }));
+        */
     }
     else
     {
         var cred = new DefaultAzureCredential();
         config.AddAzureAppConfiguration(options =>
             options.Connect(settings["AzureAppConfigConnection"]));
-                   //.ConfigureKeyVault(kv => kv.SetCredential(cred)));
+        /*
+        config.AddAzureAppConfiguration(options =>
+            options.Connect(settings["AzureAppConfigConnection"])
+                            .ConfigureKeyVault(kv => { kv.SetCredential(cred); }));
+        */
     }
 });
 */
